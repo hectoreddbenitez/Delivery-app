9996757302d6f02228c530677ee99b6f62e38193
Cassid Horimi <cassid@outlook.com.br>
2022-08-04 18:00:14 -0300

    feat: migrations, models e seeders prontas

---
+++ back-end/src/database/migrations/3-create-sales-products.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('salesProducts', {
+      saleId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onDelete: 'cascade',
+        references: {
+          model: 'sales',
+          key: 'id',
+          onDelete: 'cascade',
+        },
+        field: 'sale_id'
+      },
+      productId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onDelete: 'cascade',
+        references: {
+          model: 'products',
+          key: 'id',
+          onDelete: 'cascade',
+        },
+        field: 'product_id'
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('salesProducts');
+  }
+};
---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+  return users;
+} 
