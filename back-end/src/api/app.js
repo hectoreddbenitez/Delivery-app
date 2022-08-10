0b9f7728b695e0d282cc97b069e23270a4895e43
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-10 14:36:20 -0300

    [fix]: rota images implemented

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+// const staticZip = require('express-static-zip');
 const Routes = require('../routes');
 const errorHandler = require('../middlewares/error.handler');
 
 const app = express();
 
 app.use(cors());
+// const tset = '../../../assets/images';
+
+const dirname = path.join('..', 'assets', 'images', 'public');
+
+// app.use(staticZip(`${dirname}/images.zip`, {
+//   zipRoot: 'public/', // Use a directory inside ZIP file as
+// }));
 
 app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(Routes);
+
+app.use('/images', express.static(`${dirname}`), (req, res, next) => {
+  const options = {
+      root: path.join(dirname),
+  };
+   
+  const fileName = 'images.zip';
+  res.sendFile(fileName, options, (err) => {
+      if (err) {
+          next(err);
+      } else {
+          console.log('Sent:', fileName);
+          next();
+      }
+  }); 
+});
+
 app.use(errorHandler);
 
 module.exports = app;
