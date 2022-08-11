4849592652dc25e9cf4edb9a13d2a7cbac607020
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-11 15:10:19 -0300

    [refactor]: alterando middleware das imagens para um arquivo separado na pasta middleware.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const Routes = require('../routes');
 const errorHandler = require('../middlewares/error.handler');
+const {
+    getImagesMiddleware,
+    dirname,
+} = require('../middlewares/getImagesMiddleware');
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
 app.use(Routes);
+
+app.use('/images', express.static(`${dirname}`), getImagesMiddleware);
+
 app.use(errorHandler);
 
 module.exports = app;
