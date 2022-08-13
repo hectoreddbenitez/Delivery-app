963e4c402eb9006d758f72f4753efd33f79c8cf8
Cassid Horimi <cassid@outlook.com.br>
2022-08-12 23:05:53 -0300

    fix: arrumando o projeto - segundo ato

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const Routes = require('../routes');
 const errorHandler = require('../middlewares/error.handler');
-const {
-    getImagesMiddleware,
-    dirname,
-} = require('../middlewares/getImagesMiddleware');
 
 const app = express();
 
@@ -16,8 +12,6 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(Routes);
-
-app.use('/images', express.static(`${dirname}`), getImagesMiddleware);
 
 app.use(errorHandler);
 
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,14 +1,10 @@
 const { Router } = require('express');
 const loginRoute = require('./login');
 const registerRoute = require('./register');
-const productRoute = require('./product');
-const orderRoute = require('./order');
 
 const Routes = Router();
 
 Routes.use('/register', registerRoute);
 Routes.use('/login', loginRoute);
-Routes.use('/products', productRoute);
-Routes.use('/orders', orderRoute);
 
 module.exports = Routes;
