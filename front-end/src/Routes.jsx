dc97c75098752de19836567b6740043ffc67147e
Cassid Horimi <cassid@outlook.com.br>
2022-08-12 22:21:22 -0300

    fix: arrumando o projeto - o começo

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,10 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Products from './pages/CostumerProducts';
-import MyRequests from './pages/CostumerRequests';
-import Checkout from './pages/Checkout';
-import OrderDetails from './pages/CostumerOrderDetails';
 
 function AppRoutes() {
   return (
@@ -12,10 +8,6 @@ function AppRoutes() {
       <Route path="/" element={ <Navigate to="/login" replace /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
-      <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/customer/checkout" element={ <Checkout /> } />
-      <Route path="/customer/orders" element={ <MyRequests /> } />
-      <Route path="/customer/orders/:id" element={ <OrderDetails /> } />
     </Routes>
   );
 }
