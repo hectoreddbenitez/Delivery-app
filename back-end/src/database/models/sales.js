159ca9c635a8fb1eeb60d56f95fac1e2c48e9b59
Cassid Horimi <cassid@outlook.com.br>
2022-08-14 20:12:37 -0300

    feat: setup sequelize

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const sales = sequelize.define('sales', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: DataTypes.INTEGER,
+    sellerId: DataTypes.INTEGER,
+    totalPrice: DataTypes.DECIMAL(4, 2),
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATE,
+    status: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  sales.associete = (models) => {
+    sales.belongsTo(models.users, 
+      { foreignKey: 'userId', as: 'users', onDelete: 'CASCADE' },
+      { foreignKey: 'sellerId', as: 'sellers', onDelete: 'CASCADE' })
+  }
+  return sales;
+}  
---
+++ back-end/src/database/models/salesproducts.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const salesProducts = sequelize.define('salesProducts', {
+    salesId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    productsId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    quantity: DataTypes.INTEGER,
+  }, 
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  salesProducts.associate = (models) => {
+    models.sales.belongsToMany(models.products, {
+      through: salesProducts,
+      foreignKey: 'salesId',
+      otherKey: 'productsId',
+      as: 'sales',
+      onDelete: 'CASCADE',
+    });
+
+    models.products.belongsToMany(models.sales, {
+      through: salesProducts,
+      foreignKey: 'productsId',
+      otherKey: 'salesId',
+      as: 'products',
+      onDelete: 'CASCADE',
+    });
+  }
+
+  return salesProducts;
+}  
