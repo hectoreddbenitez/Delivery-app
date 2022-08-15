b9aa9bad17707ada1a4c7d9ffc23f74aa31ce94f
Cassid Horimi <cassid@outlook.com.br>
2022-08-14 21:12:45 -0300

    feat: refeito a tela de login

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -7,7 +7,6 @@ const loginService = async (email, pass) => {
   const user = await getUserByEmail(email);
 
   if (!user) throw ErrorConstructor(404, 'Invalid email or password');
-
   if (md5(pass) !== user.password) throw ErrorConstructor(400, 'Invalid email or password');
 
   const token = generateJWT({ name: user.name, email, role: user.role });
